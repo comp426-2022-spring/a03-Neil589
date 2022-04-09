003b79eed70d196b5ce780cce4d97da70873060a
Neil Thupili <69744850+Neil589@users.noreply.github.com>
2022-04-09 19:06:51 -0400

    h

--- server.js
+++ server.js
@@ -7,10 +7,33 @@ const server = app.listen(port, () => {
     console.log('App listening on port %PORT%'.replace('%PORT%', port))
 });
 
+function coinFlip() {
+    return Math.random() < 0.5 ? 'heads' : 'tails'
+  }
+
 app.get('/app/', (req, res) => {
     res.status(200).end('working');
     res.type('text/plain');
 });
+
+app.get('/app/echo/:number', (req, res) => {
+    res.status(200).json({'message': req.params.number});
+    
+});
+
+
+// app.get('/app/echo/', (req, res) => {
+//     res.status(200).json({"message": req.query.number});
+// });
+
+app.get('/app/echo/', (req, res) => {
+    res.status(200).json({"message": req.body.number});
+});
+
+app.get('/app/flip', (req, res) => {
+    res.status(200).json({'flip':coinFlip()});
+});
+
 
 
 app.use(function (req, res) {
